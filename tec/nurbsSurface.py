e78295fc26b3041ff8d593f6c40c1d1ac762d365
Julian Lüken <julian.lueken@stud.uni-goettingen.de>
2022-09-26 07:22:21 +0200

    poop

--- tec/nurbsSurface.py
+++ tec/nurbsSurface.py
@@ -8,10 +8,25 @@ if '-c' in sys.argv:
 # load data
 datafile = os.path.join('./nurbsSurface.dat')
 dataset = tecplot.data.load_tecplot(datafile)
+#dataset.delete_zones
 
 # settings
 frame = tecplot.active_frame()
+#frame.show_border
+#frame.active_zones(*zones) #Returns or sets the active Zones.
+#https://www.tecplot.com/docs/pytecplot/api/tecplot.data.html#zones
+
 frame.plot_type = tecplot.constant.PlotType.Cartesian3D
+
+"""
+plot.fieldmap(newzone).show = True
+plot.fieldmap(newzone).contour.show = True
+plot.fieldmap(newzone).contour.flood_contour_group = plot.contour(0)
+plot.fieldmap(newzone).edge.show = True
+plot.fieldmap(newzone).edge.line_thickness = .4
+plot.fieldmap(newzone).edge.color = Color.Orange
+"""
+
 
 # plot
 frame.plot()
